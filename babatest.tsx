9ff27797d00c994317045eba3eedc36dcb057dd2
7080 <7080@users.noreply.github.com>
2021-12-12 16:35:59 +0100

    Add files via upload

--- babatest.tsx
+++ babatest.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="115231" tilewidth="32" tileheight="32" tilecount="2112" columns="32">
- <image source="../Downloads/babatiles/baba.png" trans="000000" width="1024" height="2112"/>
+ <image source="baba.png" trans="000000" width="1024" height="2112"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="200"/>
